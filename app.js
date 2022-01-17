ae16611207765bd697842d59066effad976920d9
Utkarsh <itsutkarsh123@gmail.com>
2022-01-17 19:55:38 +0530

    add word guess function

--- app.js
+++ app.js
@@ -11,3 +11,34 @@ for (let i = 0; i < 6; i++) {
 
 	grid.appendChild(row);
 }
+
+let secretWord = "apple";
+
+let wordArr = secretWord.split("");
+
+let gridRows = grid.children;
+
+let i = 0;
+let j = 0;
+let str = "";
+
+document.addEventListener('keydown', e => {
+	if (j === 5 && e.key === 'Enter') {
+		if (str === secretWord) {
+			alert("Congo");
+			return;
+		} else {
+			j = 0;
+			i++;
+			str = '';
+			alert("Not the correct word");
+		}
+	}
+	if (e.keyCode >= 65 && e.keyCode <= 90 && j < 5) {
+		gridRows[i].children[j].textContent = e.key;
+		j++;
+		str += e.key;
+	}
+})
+
+// 
