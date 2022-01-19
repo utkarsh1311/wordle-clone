2bffda800447e55af671bb4ff20795370b0d3eb1
Utkarsh <itsutkarsh123@gmail.com>
2022-01-19 23:58:14 +0530

    add logic for fetching word from json file

--- app.js
+++ app.js
@@ -18,122 +18,131 @@ function windowOnClick(event) {
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-let grid = document.getElementById("word-grid");
+fetch("./words.json")
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		let arr = data.words;
+		let secretWord = arr[Math.floor(Math.random() * arr.length)];
+		console.log(secretWord);
+		let grid = document.getElementById("word-grid");
 
-for (let i = 0; i < 6; i++) {
-	let row = document.createElement("div");
+		for (let i = 0; i < 6; i++) {
+			let row = document.createElement("div");
 
-	for (let j = 0; j < 5; j++) {
-		let cell = document.createElement("div");
-		cell.classList.add("cell");
-		row.appendChild(cell);
-	}
+			for (let j = 0; j < 5; j++) {
+				let cell = document.createElement("div");
+				cell.classList.add("cell");
+				row.appendChild(cell);
+			}
 
-	grid.appendChild(row);
-}
-
-let secretWord = "apple";
-
-let wordArr = secretWord.split("");
-
-let gridRows = grid.children;
-
-const clearBoard = () => {
-	for (let i = 0; i < 6; i++) {
-		for (let j = 0; j < 5; j++) {
-			gridRows[i].children[j].textContent = "";
-			gridRows[i].children[j].classList.remove(
-				"correct-cell",
-				"correct-letter-wrong-cell",
-				"wrong-cell"
-			);
+			grid.appendChild(row);
 		}
-	}
-};
+		let wordArr = secretWord.split("");
 
-let row = 0;
-let col = 0;
-let str = "";
-let modalText = document.querySelector(".modal-text");
-let modalContent = document.querySelector(".modal-content");
+		let gridRows = grid.children;
 
-const resetGame = () => {
-	clearBoard();
-	row = 0;
-	col = 0;
-	str = "";
-	toggleModal();
-};
-
-document.addEventListener("keydown", (e) => {
-	if (e.key === "Backspace" && col >= 1) {
-		col--;
-		gridRows[row].children[col].textContent = "";
-		str = str.substring(0, str.length - 1);
-	}
-
-	if (col === 5 && e.key === "Enter") {
-		if (str === secretWord) {
-			modalText.innerHTML =
-				"<h1> Congratulations 🎉🎉🎉 <br>You guesses the correct word. <h1>";
-			allCorrect(row);
-			toggleModal();
-			// setTimeout(toggleModal, 3000);
-			setTimeout(resetGame, 3000);
-		} else {
-			let wrongArr = str.split("");
-			wrongWordColor(wrongArr, row);
-			col = 0;
-			row++;
-			str = "";
-		}
-	}
-	if (e.keyCode >= 65 && e.keyCode <= 90 && col < 5) {
-		gridRows[row].children[col].textContent = e.key.toUpperCase();
-		col++;
-		str += e.key.toLowerCase();
-	}
-	
-	if (row === 6) {
-		modalContent.innerHTML =
-			'<img class="game-over" src="./game-over-removebg-preview.png">';
-		toggleModal();
-		setTimeout(resetGame, 3000);
-	}
-});
-
-const allCorrect = (row) => {
-	for (let i = 0; i < 5; i++) {
-		gridRows[row].children[i].classList.add("correct-cell");
-	}
-};
-
-const wrongWordColor = (arr, row) => {
-	let cells = gridRows[row].children;
-	for (let i = 0; i < 5; i++) {
-		if (wordArr[i] === arr[i]) {
-			cells[i].classList.add("correct-cell");
-		}
-	}
-
-	for (let i = 0; i < 5; i++) {
-		for (let j = 0; j < 5; j++) {
-			if (wordArr[i] === arr[j]) {
-				if (!cells[j].classList.contains("correct-cell")) {
-					cells[j].classList.add("correct-letter-wrong-cell");
+		const clearBoard = () => {
+			for (let i = 0; i < 6; i++) {
+				for (let j = 0; j < 5; j++) {
+					gridRows[i].children[j].textContent = "";
+					gridRows[i].children[j].classList.remove(
+						"correct-cell",
+						"correct-letter-wrong-cell",
+						"wrong-cell"
+					);
 				}
 			}
-		}
-	}
+		};
 
-	for (let i = 0; i < 5; i++) {
-		if (
-			!(
-				cells[i].classList.contains("correct-cell") ||
-				cells[i].classList.contains("correct-letter-wrong-cell")
-			)
-		) {
-			cells[i].classList.add("wrong-cell");
-		}
-	}
-};
+		let row = 0;
+		let col = 0;
+		let str = "";
+		let modalText = document.querySelector(".modal-text");
+		let modalContent = document.querySelector(".modal-content");
+
+		const resetGame = () => {
+			clearBoard();
+			row = 0;
+			col = 0;
+			str = "";
+			toggleModal();
+		};
+
+		document.addEventListener("keydown", (e) => {
+			if (e.key === "Backspace" && col >= 1) {
+				col--;
+				gridRows[row].children[col].textContent = "";
+				str = str.substring(0, str.length - 1);
+			}
+
+			if (col === 5 && e.key === "Enter") {
+				if (str === secretWord) {
+					modalText.innerHTML =
+						"<h1> Congratulations 🎉🎉🎉 <br>You guesses the correct word. <h1>";
+					allCorrect(row);
+					toggleModal();
+					// setTimeout(toggleModal, 3000);
+					setTimeout(resetGame, 3000);
+				} else {
+					let wrongArr = str.split("");
+					wrongWordColor(wrongArr, row);
+					col = 0;
+					row++;
+					str = "";
+				}
+			}
+			if (e.keyCode >= 65 && e.keyCode <= 90 && col < 5) {
+				gridRows[row].children[col].textContent = e.key.toUpperCase();
+				col++;
+				str += e.key.toLowerCase();
+			}
+
+			if (row === 6) {
+				modalContent.innerHTML =
+					'<img class="game-over" src="./game-over-removebg-preview.png">';
+				toggleModal();
+				setTimeout(resetGame, 3000);
+			}
+		});
+
+		const allCorrect = (row) => {
+			for (let i = 0; i < 5; i++) {
+				gridRows[row].children[i].classList.add("correct-cell");
+			}
+		};
+
+		const wrongWordColor = (arr, row) => {
+			let cells = gridRows[row].children;
+			for (let i = 0; i < 5; i++) {
+				if (wordArr[i] === arr[i]) {
+					cells[i].classList.add("correct-cell");
+				}
+			}
+
+			for (let i = 0; i < 5; i++) {
+				for (let j = 0; j < 5; j++) {
+					if (wordArr[i] === arr[j]) {
+						if (!cells[j].classList.contains("correct-cell")) {
+							cells[j].classList.add("correct-letter-wrong-cell");
+						}
+					}
+				}
+			}
+
+			for (let i = 0; i < 5; i++) {
+				if (
+					!(
+						cells[i].classList.contains("correct-cell") ||
+						cells[i].classList.contains("correct-letter-wrong-cell")
+					)
+				) {
+					cells[i].classList.add("wrong-cell");
+				}
+			}
+		};
+	})
+	.catch((err) => {
+		console.log("Error in fetching word");
+	});
